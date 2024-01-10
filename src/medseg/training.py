ea1176bfaf10aae2bcc1a4c33cd43222a63292f1
Martin Lund Trinhammer <martintrinhammer@hotmail.com>
2024-01-10 11:13:45 +0100

    training loop adjusted to incorporate validatoin metrics

--- src/medseg/training.py
+++ src/medseg/training.py
@@ -17,7 +17,7 @@ def train(model, opt, loss_fn, epochs, train_loader, test_loader, device):
     for epoch in range(epochs):
         tic = time()
         LOG.info('* Epoch test %d/%d' % (epoch+1, epochs))
-
+        
         avg_loss = 0
         model.train()  # train mode
         for X_batch, Y_batch in train_loader:
@@ -38,6 +38,7 @@ def train(model, opt, loss_fn, epochs, train_loader, test_loader, device):
 
         model.eval()  
         val_loss = 0
+        dice_loss_val, iou_loss_val, acc_val, sens_val, spec_val = 0, 0, 0, 0, 0
         with torch.no_grad():  
             for X_val, Y_val in val_loader:
                 X_val = X_val.to(device)
@@ -47,8 +48,23 @@ def train(model, opt, loss_fn, epochs, train_loader, test_loader, device):
                 loss_val = loss_fn(Y_pred_val, Y_val)
                 val_loss += loss_val.item() * X_val.size(0)
 
-        avg_loss = avg_loss / len(train_loader.dataset)
-        val_loss = val_loss / len(val_loader.dataset)
+
+                Y_pred_binary = torch.round(Y_pred_val)
+
+                dice_loss_val += dice_coefficient(Y_pred_binary, Y_val)
+                iou_loss_val += iou_loss(Y_pred_binary, Y_val)
+                acc_val += accuracy(Y_pred_binary, Y_val)
+                sens_val += sensitivity(Y_pred_binary, Y_val)
+                spec_val += specificity(Y_pred_binary, Y_val)
+
+        # Average out the metrics over the entire validation dataset
+        num_batches = len(val_loader)
+        val_loss /= len(val_loader.dataset)
+        dice_loss_val /= num_batches
+        iou_loss_val /= num_batches
+        acc_val /= num_batches
+        sens_val /= num_batches
+        spec_val /= num_batches
 
         toc = time()
         LOG.info(' - loss: %f' % avg_loss)
@@ -67,3 +83,4 @@ def train(model, opt, loss_fn, epochs, train_loader, test_loader, device):
             plt.axis('off')
         plt.suptitle('%d / %d - loss: %f' % (epoch+1, epochs, avg_loss))
         plt.show()
+    return dice_loss_val.item(), iou_loss_val.item(), acc_val.item(), sens_val.item(), spec_val.item()
