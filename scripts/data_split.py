4f39dbe674d98a10e5bfa1fcaf35244f94efefae
Benjamin <s236771@student.dtu.dk>
2024-01-11 17:35:35 +0100

    Fix syntax error caused by unintended tabs

--- scripts/data_split.py
+++ scripts/data_split.py
@@ -13,25 +13,25 @@ folders = [f for f in os.listdir(path) if os.path.isdir(os.path.join(path, f))]
 random.shuffle(folders)
 
 # Split ratios for train, test, validation
-	train_ratio = 0.7
-	test_ratio = 0.15
+train_ratio = 0.7
+test_ratio = 0.15
 # Validation ratio is implied
 
 # Calculate split indices
-	total_folders = len(folders)
-	train_end = int(train_ratio * total_folders)
-	test_end = train_end + int(test_ratio * total_folders)
+total_folders = len(folders)
+train_end = int(train_ratio * total_folders)
+test_end = train_end + int(test_ratio * total_folders)
 
 # Split the list
-	train_folders = folders[:train_end]
-	test_folders = folders[train_end:test_end]
-	validation_folders = folders[test_end:]
+train_folders = folders[:train_end]
+test_folders = folders[train_end:test_end]
+validation_folders = folders[test_end:]
 
 # Function to write folders to a file
-	def write_to_file(folders, file_name):
-		    with open(file_name, 'w') as f:
-					        for folder in folders:
-									            f.write("%s\n" % folder)
+def write_to_file(folders, file_name):
+    with open(file_name, 'w') as f:
+        for folder in folders:
+            f.write("%s\n" % folder)
 
 # Write to respective files
 write_to_file(train_folders, '/zhome/25/e/155273/Desktop/02516_dvcv/02516_intro_cnn_w2/train.txt')
